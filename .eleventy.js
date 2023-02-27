56dd4414723139b8a5f84dd42d1b3eb1a37eb821
Dan Jordan <daniel.jordan@mindera.com>
2023-02-27 14:53:43 +0000

    Eleventy 2 upgrade and cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,14 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginInclusiveLanguage = require("@11ty/eleventy-plugin-inclusive-language");
 
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addCollection("pages", (collectionApi) => {
+    return collectionApi.getAllSorted().filter((item) => {
+      // Remove css files that get added by addExtension
+      const isCss = item.data.page.outputFileExtension === "css";
+      return !isCss;
+    });
+  });
+
   eleventyConfig.addFilter("take", (arr = [], num) => {
     return arr.slice(0, num);
   });
@@ -32,18 +40,20 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addTemplateFormats("scss");
   eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css", // optional, default: "html"
+    outputFileExtension: "css",
 
     // `compile` is called once per .scss file in the input directory
     compile: function (inputContent, inputPath) {
-      let parsed = path.parse(inputPath);
+      const parsed = path.parse(inputPath);
 
-      if (parsed.base.startsWith("_")) return;
+      if (parsed.name.startsWith("_")) return;
 
-      let result = sass.compileString(inputContent, {
+      const result = sass.compileString(inputContent, {
         loadPaths: [parsed.dir || ".", this.config.dir.includes],
         style: "compressed",
       });
+
+      this.addDependencies(inputPath, result.loadedUrls);
 
       // This is the render function, `data` is the full data cascade
       return () => {
@@ -54,29 +64,14 @@ module.exports = (eleventyConfig) => {
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (outputPath && outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
+      return htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
       });
-      return minified;
     }
 
     return content;
-  });
-
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: (err, browserSync) => {
-        const content_404 = fs.readFileSync("_site/404.html");
-
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
   });
 
   return {
