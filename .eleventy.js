6a409767b34b3a080a45259017318c43782bc426
Dan Jordan <daniel.jordan@mindera.com>
2023-03-23 15:05:20 +0000

    Use plugins for sass to get rev in url

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,7 @@
-const sass = require("sass");
-const fs = require("node:fs");
-const path = require("node:path");
 const htmlmin = require("html-minifier");
 const format = require("date-fns/format");
+const pluginRev = require("eleventy-plugin-rev");
+const pluginEleventySass = require("eleventy-sass");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const zonedTimeToUtc = require("date-fns-tz/zonedTimeToUtc");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
@@ -27,6 +26,10 @@ module.exports = (eleventyConfig) => {
   });
 
   eleventyConfig.addPlugin(pluginInclusiveLanguage);
+  eleventyConfig.addPlugin(pluginEleventySass, {
+    rev: true,
+  });
+  eleventyConfig.addPlugin(pluginRev);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
 
@@ -37,30 +40,6 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("src/humans.txt");
   eleventyConfig.addPassthroughCopy("src/manifest.json");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
-
-  eleventyConfig.addTemplateFormats("scss");
-  eleventyConfig.addExtension("scss", {
-    outputFileExtension: "css",
-
-    // `compile` is called once per .scss file in the input directory
-    compile: function (inputContent, inputPath) {
-      const parsed = path.parse(inputPath);
-
-      if (parsed.name.startsWith("_")) return;
-
-      const result = sass.compileString(inputContent, {
-        loadPaths: [parsed.dir || ".", this.config.dir.includes],
-        style: "compressed",
-      });
-
-      this.addDependencies(inputPath, result.loadedUrls);
-
-      // This is the render function, `data` is the full data cascade
-      return () => {
-        return result.css;
-      };
-    },
-  });
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (outputPath && outputPath.endsWith(".html")) {
